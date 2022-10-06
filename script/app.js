eeb37f7553be44ce792e457e96755bad01360039
VanderschuerenMaximilian <91435601+VanderschuerenMaximilian@users.noreply.github.com>
2022-10-06 15:42:43 +0200

    password toggle

--- script/app.js
+++ script/app.js
@@ -1,9 +1,26 @@
+const password = {}
+
 function handleFloatingLabel() {
    
 }
 
 function handlePasswordSwitcher() {
-    
+  password.input = document.querySelector('.js-password-input');
+  password.toggle = document.querySelector('.js-password-toggle');
+  password.hide = document.querySelector('.c-toggle-password__icon--hide');
+  password.show = document.querySelector('.c-toggle-password__icon--show');
+
+  password.toggle.addEventListener('change', function () {
+    if (password.toggle.checked) {
+      password.input.type = 'input';
+      password.hide.classList.add('o-hide-accessible');
+      password.show.classList.remove('o-hide-accessible');
+    } else {
+      password.input.type = 'password';
+      password.hide.classList.remove('o-hide-accessible');
+      password.show.classList.add('o-hide-accessible');
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
